cb63d75495293c894101ac7bb7a7f0f1f304f25f
Mike Willbanks <mike@digitalstruct.com>
2012-01-26 14:48:34 -0600

    added form text view helper to allow for prepending and appending text fields, added configuration for the view renderers, however, some work is needed in the actual form... will likely set the view to happen differently later

--- config/module.config.php
+++ config/module.config.php
@@ -8,7 +8,16 @@ return array(
                         'formSubmit' => 'TwitterBootstrapFormDecorators\View\Helper\FormSubmit',
                         'formButton' => 'TwitterBootstrapFormDecorators\View\Helper\FormButton',
                         'formErrors' => 'TwitterBootstrapFormDecorators\View\Helper\FormErrors',
+                        'formText'   => 'TwitterBootstrapFormDecorators\View\Helper\FormText',
                     ),
+                ),
+            ),
+            'Zend\View\PhpRenderer' => array(
+                'broker' => 'Zend\View\HelperBroker',
+            ),
+            'Zend\View\HelperBroker' => array(
+                'parameters' => array(
+                    'loader' => 'Zend\View\HelperLoader',
                 ),
             ),
         ),
---
+++ src/TwitterBootstrapFormDecorators/View/Helper/FormText.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * TwitterBootstrapFormDecorators
+ *
+ * @category   TwitterBootstrapFormDecorators
+ * @package    TwitterBootstrapFormDecorators\View
+ * @subpackage Helper
+ */
+
+/**
+ * @namespace
+ */
+namespace TwitterBootstrapFormDecorators\View\Helper;
+
+/**
+ * Helper to generate a "text" element
+ *
+ * @uses       \Zend\View\Helper\FormText
+ * @category   TwitterBoostrapFormDecorators
+ * @package    TwitterBootstrapFormDecorators\View
+ * @subpackage Helper
+ */
+class FormText extends \Zend\View\Helper\FormText
+{
+    /**
+     * Generates a 'text' element.
+     *
+     * @access public
+     *
+     * @param string|array $name If a string, the element name.  If an
+     * array, all other parameters are ignored, and the array elements
+     * are used in place of added parameters.
+     *
+     * @param mixed $value The element value.
+     *
+     * @param array $attribs Attributes for the element tag.
+     *
+     * @return string The element XHTML.
+     */
+    public function __invoke($name, $value = null, $attribs = null)
+    {
+        $info = $this->_getInfo($name, $value, $attribs);
+        extract($info); // name, value, attribs, options, listsep, disable
+
+        $prepend = '';
+        if (isset($attribs['prepend'])) {
+            $prepend = '<span class="add-on">'
+                . $this->view->vars()->escape($attribs['prepend'])
+                . '</span>';
+            unset($attribs['prepend']);
+        }
+
+        $append = '';
+        if (isset($attribs['append'])) {
+            $append = '<span class="add-on">'
+                . $this->view->vars()->escape($attribs['append'])
+                . '</span>';
+            unset($attribs['append']);
+        }
+
+
+        return $prepend . parent::__invoke($name, $value, $attribs) . $append;
+    }
+}
